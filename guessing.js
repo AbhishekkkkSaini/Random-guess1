e6d9ec6ecc7a7aa215e2e1d98abb93495ba27d26
AbhishekkkkSaini <singhaniaeditings@gmail.com>
2024-12-26 22:22:55 +0530

    Update guessing.js

--- guessing.js
+++ guessing.js
@@ -5,4 +5,8 @@ while(usernum!=gamenum){
     usernum=prompt("You guessed the wrong number , Please try again : ");
 }
 
+console.log("Abhishek saini")
+//this is my change 
+
+
 console.log("Conguratulations , You guessed the right number.")
